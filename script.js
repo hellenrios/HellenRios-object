d8943e2baae53f78f7929507d45f0bce092bbba6
Hellen Rios <hellenrioss@gmail.com>
2022-02-03 20:30:45 -0300

    adicionando exercicio sobre objetos

---
+++ script.js
@@ -0,0 +1,17 @@
+// Crie um objeto que receba ao menos três propriedades sobre você.
+let person = {
+    name: "Hellen",
+    age: 23,
+    city: "Rio de Janeiro"
+}
+
+console.log(person)
+
+// Adicione uma nova propriedade sem alterar seu objeto inicial.
+person.street = "Rua da Glória"
+
+// Remova uma propriedade desse objeto.
+delete person.age
+
+//Mostre no console todas as propriedades desse objeto.
+console.log(person)
